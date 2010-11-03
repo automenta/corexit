f97eb6de5ccbe7a05293a4a196e3293ef22528d8
SeH <seh999@gmail.com>
2010-11-03 01:38:29 -0400

    more improvements

--- afterLoad.js
+++ afterLoad.js
@@ -1,16 +1,17 @@
 
-document.body.innerHTML =
-    '<div id="_corexitOverlay"' +
-    'style="position:fixed;top:5%;left:5%;text-align:left;z-index:99999999;width:90%;height:90%;display:none;background-color: gray;border:black 1px solid;color:white;font-family:Arial,Sans;overflow:none">' +
-    '  <div id="_corexitTop" style="font-size:20px">'+
-    '    <div style="text-align: right">' +
-    '      <a href="#" style="color:white;" onClick="document.getElementById(\'_corexitOverlay\').style.display=\'none\';">X</a>' +
-    '    </div>' +
-    '  </div>' +
-    '  <div id="_corexitOverlayContent">' +
-    '  </div>' +
-    '</div>' +
-    document.body.innerHTML;
+//document.body.innerHTML =
+//    document.body.innerHTML +
+//    '<div id="_corexitOverlay"' +
+//    'style="position:fixed;top:5%;left:5%;text-align:left;z-index:99999999;width:100%;height:100%;display:none;background-color: gray;border:black 1px solid;color:white;font-family:Arial,Sans;overflow:none">' +
+//    '  <div id="_corexitTop" style="font-size:20px">'+
+//    '    <div style="text-align: right">' +
+//    '      <a href="#" style="color:white;" onClick="document.getElementById(\'_corexitOverlay\').style.display=\'none\';">X</a>' +
+//    '    </div>' +
+//    '  </div>' +
+//    '  <div id="_corexitOverlayContent">' +
+//    '  </div>' +
+//    '</div>'
+//    ;
 
 
 // Catch mouse down and up events
